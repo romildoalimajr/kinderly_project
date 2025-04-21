7f6962541f1b0a451efbe009c568dc157ea971e2
Romildo A. Lima Jr. <odlimor.sevla@gmail.com>
2025-04-20 21:10:53 -0300

    Implementado a função no javaScript para activities..

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
-Document.addEventListner('DOMContentLoaded', function(){
-    console.log('JS attached');
+document.addEventListener('DOMContentLoaded', function(){
+    /**
+     * Activities titles
+     */
+    let activitiesTitles = document.querySelectorAll(
+        ".activities__title__button"
+    );
+    activitiesTitles.forEach((title) => {
+        //console.log(title);
+        title.addEventListener('click', (t) => {
+            //console.log(t);
+            activitiesTitles.forEach((t) => t.classList.remove('active'));
+            title.classList.add('active');
+
+            let activitiesList = document.querySelectorAll('.activities__item');
+            activitiesList.forEach((item) => {
+                //console.log(item);
+                item.style.display = "none";
+                if(item.dataset.type === title.dataset.type || title.dataset.type === 'all'){
+                    item.style.display = "block";
+                }
+            });
+        });
+    });
 });
